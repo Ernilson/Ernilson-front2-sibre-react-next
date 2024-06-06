8a10ba4343ebc4cd1316633fe1961bec85bf0faa
ernilson <ernilson25@gmail.com>
2024-06-06 07:52:23 -0300

    Corrigido o texto na paginga Sobre

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a65a4cf7b9458d93"; }
+/******/ 		__webpack_require__.h = function() { return "86fe6deab6ee72bf"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/sobre.js
+++ pages/sobre.js
@@ -38,14 +38,13 @@ function sobre() {
                 <div className="row featurette">
                     <div className="col-md-7 order-md-2">
                         <h2 className="featurette-heading">Segunda Igreja Batista no Recanto das Emas - DF.</h2>
-                        <p className="lead">A Segunda Igreja Batista foi organizada como igreja em 2009.  Esta era congregação
-                        da Segunda Igreja Batista do Plano Piloto e ficava localizada na quadra 103 quando, em 2003, a SIBPP adquiriu
-                        um lote para a construção de um lugar próprio e definitivo. A construção do templo foi realizada pela Segunda
-                        Igreja Batista do Plano Piloto e contou com a ajuda de um grupo de irmãos norte americanos vindos ao Brasil com esta
-                        finalidade. A Sibre está localizada na quadra 105 área especial 01 - Avenida Vargem da Benção.
-                        Ao relembrarmos nossa história, algumas pessoas foram determinantes para que a nossa igreja fosse estabelecida e estas contribuíram em nosso
-                        crescimento. Vale mencionar Pr. Jaime França e sua família, Pr. Alexandre e sua esposa Raquel Mousinho de Moura Fé, Pr. Daniel Ventura, Pr. Edvan Xavier, Pr. Regivaldo Dantas e Pr. Josias Alves da Costa.
-                        Hoje a Sibre é pastoreada pelo Pr.Lacir com o auxilio dos seminaristas: Jairo Guimarães, Lucio Macedo e Gostavo Lenor.
+                        <p className="lead">A Segunda Igreja Batista foi organizada como igreja no dia 31 de maio de 2003, anteriormente sendo uma congregação da Segunda Igreja Batista do Plano Piloto.
+                             Inicialmente, ficava localizada na quadra 103 do Recanto das Emas. Naquela época, a SIBPP adquiriu um lote para a construção de um local próprio e definitivo para a congregação.
+A construção do templo foi realizada pela Segunda Igreja Batista do Plano Piloto, contando com a ajuda de um grupo de irmãos norte-americanos que vieram ao Brasil com essa finalidade. Atualmente,
+a SIBRE está localizada na quadra 105, área especial 01 - Avenida Vargem da Benção.
+Ao relembrarmos nossa história, algumas pessoas foram determinantes para o estabelecimento e crescimento de nossa igreja. Entre elas, destacamos: Pr. Jaime França e sua família, Pr. Alexandre e sua esposa Raquel Mousinho de Moura Fé,
+Pr. Daniel Ventura, Pr. Edvan Xavier, Pr. Regivaldo Dantas e Pr. Josias Alves da Costa.
+Hoje, a SIBRE é pastoreada pelo Pr. Lacir, com o auxílio dos seminaristas Jairo Guimarães e Gustavo Lenor..
 </p>
                     </div>
                     <div className="col-md-5 order-md-1">
@@ -60,7 +59,9 @@ function sobre() {
                     <div className="col-md-7 order-md-2">
                         <h2 className="featurette-heading">Missão</h2>
                         <p className="lead">Amar a Deus, servir ao próximo, fazer o maior número discípulos
-                         até a volta de Cristo e ser uma igreja relevante para sua comunidade. </p>
+                         até a volta do Senhor Jesus Cristo. <br/> "E disse-lhes: Ide por todo o mundo, e pregai o evangelho a toda criatura".
+                         Marcos 16:15
+                          </p>                         
                     </div>
                     <div className="col-md-5 order-md-1">
                         <img src="/img007.png" className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
